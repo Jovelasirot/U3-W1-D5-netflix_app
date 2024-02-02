aa587bc6a07adc18da31f9193abd258e99f15491
Jovelasirot <jovelasirot@gmail.com>
2024-02-02 16:12:00 +0100

    search added

---
+++ src/components/Search.jsx
@@ -0,0 +1,110 @@
+import React, { Component } from "react";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import { Button, Spinner } from "react-bootstrap";
+
+class Search extends Component {
+  state = {
+    films: [],
+    error: null,
+    searchValue: "",
+    loading: false,
+    selected: false,
+  };
+
+  fetchFilms = () => {
+    const filmUrl = "http://www.omdbapi.com/?apikey=f690f01f&s=";
+    const { searchValue } = this.state;
+    fetch(filmUrl + searchValue)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.Response === "True") {
+          this.setState({ films: data.Search, loading: false });
+        } else {
+          this.setState({
+            films: [],
+            loading: false,
+            error: "Something went wrong while searching your film (╯•ᗣ•╰)",
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching films:", error);
+        this.setState({ error: error.message, loading: false });
+      });
+  };
+
+  handleSearch = () => {
+    this.setState({ loading: true, error: null });
+    this.fetchFilms();
+  };
+
+  render() {
+    const { films, loading, error } = this.state;
+
+    return (
+      <Container fluid className="pt-4 bg-primary text-light pb-5">
+        <Row>
+          <Col>
+            <h4 className="text-start">Find your favorite film</h4>
+          </Col>
+          <Col sm={1}>
+            <Form>
+              <Form.Control
+                type="text"
+                placeholder="Find a film"
+                value={this.state.searchValue}
+                onChange={(e) => {
+                  this.setState({ searchValue: e.target.value });
+                }}
+              />
+            </Form>
+          </Col>
+          <Col xs="auto">
+            <Button onClick={this.handleSearch} variant="dark rounder-0">
+              Search a film
+            </Button>
+          </Col>
+        </Row>
+        <Row className="justify-content-center mt-3 g-3 mx-3 mx-md-0">
+          {loading ? (
+            <Spinner animation="border" variant="warning"></Spinner>
+          ) : error ? (
+            <Col xs={12}>
+              <p>Error: {error}</p>
+            </Col>
+          ) : (
+            <>
+              <Row
+                xs={1}
+                md={2}
+                lg={3}
+                xl={6}
+                className="justify-content-center gy-5 "
+              >
+                {films && films.length > 0 ? (
+                  films.map((film) => (
+                    <Col key={film.imdbID}>
+                      <img
+                        src={film.Poster}
+                        alt={film.Title}
+                        className="h-100 w-75"
+                      />
+                      <div>{film.Title}</div>
+                    </Col>
+                  ))
+                ) : (
+                  <p>Waiting for you to search</p>
+                )}
+              </Row>
+            </>
+          )}
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Search;
